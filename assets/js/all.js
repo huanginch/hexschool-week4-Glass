2bf38c38d347bc2bfbaf2df6ece570b70718068c
huanginch <huanginch329@gmail.com>
2022-07-31 15:09:28 +0800

    Update 2022-07-31T07:09:07.104Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,12 +1,13 @@
-// console.log('Hello!');
+"use strict";
 
-$(document).ready(() => {
+// console.log('Hello!');
+$(document).ready(function () {
   $('.menu-btn').click(function (e) {
     e.preventDefault();
     $('.navbar').toggleClass('show');
   });
 });
-
 $('.navbar li a').click(function (e) {
   $('.navbar').removeClass('show');
-})
+});
+//# sourceMappingURL=all.js.map
